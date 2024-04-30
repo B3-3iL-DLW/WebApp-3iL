81456940ab31a51bc74235993da59d31ecd04a8e
Lukas <lukasvalois@gmail.com>
2024-04-30 15:52:05 +0200

    :bulb: Added comments in persist.service.ts

--- backend-nest/src/api/persist/persist.service.ts
+++ backend-nest/src/api/persist/persist.service.ts
@@ -13,7 +13,9 @@ export class PersistService {
     private readonly classGroupService: ClassgroupsService,
     private readonly TimetableService: TimetableService,
   ) {}
-
+  /**
+   * Cron job to persist classes every 5 minutes.
+   */
   @Cron('*/5 * * * *')
   persistClasses() {
     this.apiService.getClasses().subscribe((response) => {
@@ -30,58 +32,98 @@ export class PersistService {
       });
     });
   }
-
+  /**
+   * Cron job to persist events every 10 seconds.
+   */
   @Cron('*/10 * * * * *')
   persistEvents() {
     this.classGroupService.findAll().then((classGroups) => {
       classGroups.forEach((classGroup: { name: string; id: any }) => {
-        this.apiService.getTimetableByClassFile(classGroup.name).subscribe({
-          next: (response: AxiosResponse<any>) => {
-            response.data.forEach(
-              (item: { code: string; DaySchedule: any[] }) => {
-                item.DaySchedule.forEach(
-                  (daySchedule: { date: any; events: any[] }) => {
-                    daySchedule.events.forEach(
-                      (event: {
-                        creneau: number;
-                        horaire: any;
-                        activite: string;
-                        couleur: string;
-                        salle: string;
-                        visio: boolean;
-                        repas: boolean;
-                        eval: boolean;
-                        startAt: any;
-                        endAt: any;
-                      }) => {
-                        const data = {
-                          creneau: event.creneau,
-                          semaine: item.code,
-                          dateJour: new Date(
-                            new Date(daySchedule.date).setHours(0, 0, 0, 0),
-                          ).toISOString(),
-                          activite: event.activite,
-                          couleur: event.couleur,
-                          salle: event.salle,
-                          visio: event.visio,
-                          repas: event.repas,
-                          eval: event.eval,
-                          startAt: event.horaire.startAt,
-                          endAt: event.horaire.endAt,
-                          classGroupId: classGroup.id,
-                        };
-                        this.TimetableService.upsert(data);
-                      },
-                    );
-                  },
-                );
-              },
-            );
-          },
-          error: (error) => console.log(error),
-          complete: () => console.info('complete'),
-        });
+        this.processClassGroup(classGroup);
       });
     });
+  }
+  /**
+   * Process a class group.
+   * @param {Object} classGroup - The class group to process.
+   */
+  processClassGroup(classGroup: { name: string; id: any }) {
+    this.apiService.getTimetableByClassFile(classGroup.name).subscribe({
+      next: (response: AxiosResponse<any>) => {
+        this.processResponse(response, classGroup);
+      },
+      error: (error) => console.log(error),
+      complete: () => console.info('complete'),
+    });
+  }
+  /**
+   * Process a response.
+   * @param {AxiosResponse} response - The response to process.
+   * @param {Object} classGroup - The class group associated with the response.
+   */
+  processResponse(
+    response: AxiosResponse<any>,
+    classGroup: { name: string; id: any },
+  ) {
+    response.data.forEach((item: { code: string; DaySchedule: any[] }) => {
+      this.processItem(item, classGroup);
+    });
+  }
+  /**
+   * Process an item.
+   * @param {Object} item - The item to process.
+   * @param {Object} classGroup - The class group associated with the item.
+   */
+  processItem(
+    item: { code: string; DaySchedule: any[] },
+    classGroup: { name: string; id: any },
+  ) {
+    item.DaySchedule.forEach((daySchedule: { date: any; events: any[] }) => {
+      this.processDaySchedule(daySchedule, item, classGroup);
+    });
+  }
+  /**
+   * Process a day schedule.
+   * @param {Object} daySchedule - The day schedule to process.
+   * @param {Object} item - The item associated with the day schedule.
+   * @param {Object} classGroup - The class group associated with the day schedule.
+   */
+  processDaySchedule(
+    daySchedule: { date: any; events: any[] },
+    item: { code: string; DaySchedule: any[] },
+    classGroup: { name: string; id: any },
+  ) {
+    daySchedule.events.forEach(
+      (event: {
+        creneau: number;
+        horaire: any;
+        activite: string;
+        couleur: string;
+        salle: string;
+        visio: boolean;
+        repas: boolean;
+        eval: boolean;
+        startAt: any;
+        endAt: any;
+      }) => {
+        const data = {
+          creneau: event.creneau,
+          semaine: item.code,
+          dateJour: new Date(
+            new Date(daySchedule.date).setHours(0, 0, 0, 0),
+          ).toISOString(),
+          activite: event.activite,
+          couleur: event.couleur,
+          salle: event.salle,
+          visio: event.visio,
+          repas: event.repas,
+          eval: event.eval,
+          startAt: event.horaire.startAt,
+          endAt: event.horaire.endAt,
+          classGroupId: classGroup.id,
+        };
+        this.TimetableService.upsert(data);
+      },
+    );
   }
 }
