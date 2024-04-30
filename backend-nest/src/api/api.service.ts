27f428dda5d99870fbbe5473ac899b3477ca33b2
Lukas <lukasvalois@gmail.com>
2024-04-30 15:57:30 +0200

    :bulb: Added comment to api.service.ts

--- backend-nest/src/api/api.service.ts
+++ backend-nest/src/api/api.service.ts
@@ -4,18 +4,33 @@ import { classgroup } from '@prisma/client';
 import { Observable } from 'rxjs';
 import { AxiosResponse } from 'axios';
 
+/**
+ * Service for handling API requests.
+ */
 @Injectable()
 export class ApiService {
+  /**
+   * @param {HttpService} httpService - The HTTP service.
+   */
   constructor(private readonly httpService: HttpService) {}
 
+  /**
+   * Get all classes.
+   * @returns {Observable<AxiosResponse<classgroup[]>>} An Observable that contains the response from the API.
+   */
   getClasses(): Observable<AxiosResponse<classgroup[]>> {
     return this.httpService.get<classgroup[]>(
       'https://api.lukasvalois.com/api/classes',
     );
   }
 
+  /**
+   * Get timetable by class file.
+   * @param {string} file - The name of the class file.
+   * @returns {Observable<AxiosResponse<any>>} An Observable that contains the response from the API.
+   */
   getTimetableByClassFile(file: string): Observable<AxiosResponse<any>> {
-    // On encode le paramètre de la requête pour éviter les erreurs
+    // Encode the request parameter to avoid errors
     file = encodeURIComponent(file);
     return this.httpService.get<any>(
       `https://api.lukasvalois.com/api/timetable?class_param=${file}`,
